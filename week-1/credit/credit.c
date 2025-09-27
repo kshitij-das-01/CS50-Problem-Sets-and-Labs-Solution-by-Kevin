cf019e5c90273ac97657973bb0df2e6256f42281
KD_1501 <146980601+kshitij-das-01@users.noreply.github.com>
2025-09-27 20:39:41 +0000

    Week 1 - Credit [without function]

---
+++ week-1/credit/credit.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Input for the credit card number, also prompting user for non negative input
+    long num = 0;
+    do
+    {
+        printf("Number: ");
+        scanf("%ld", &num);
+    }
+    while (num < 0);
+    
+    // get the number of digits in the number
+    long cc = num;
+    int len = 0;
+
+    while (cc > 0)
+    {
+        cc = cc / 10;
+        len++;
+    }
+
+    // validity of credit card number as per length of digits
+    if (len != 13 && len != 15 && len != 16)
+    {
+        printf("Invalid Input\n");
+        return 1;
+    }
+
+    // variables for the formula calculation
+    long x = num;
+    int last1 = 0;
+    int last2 = 0;
+    int digi1 = 0;
+    int digi2 = 0;
+    int sum1 = 0;
+    int sum2 = 0;
+
+    // Luhn algorithm for check sum of the credit card numbers
+    for (int i = 0; i < len; i++)
+    {
+        // sum of digit from last digit and further alternating
+        last1 = x % 10;
+        x = x / 10;
+        sum1 = sum1 + last1;
+
+        // sum of digit from 2nd last digit and further alternating
+        last2 = x % 10;
+        x = x / 10;
+
+        last2 = last2 * 2;
+        digi1 = last2 % 10;
+        digi2 = last2 / 10;
+
+        sum2 = sum2 + digi1 + digi2;
+    }
+
+    // total of the sum of the above calculation
+    int total = 0;
+    total = sum1 + sum2;
+
+    // Checking if the modulo is equal to 0
+    if (total % 10 != 0)
+    {
+        printf("Invalid Credit Card Number\n");
+        return 2;
+    }
+
+    // Variables to check the digits of from the start
+    long start = num;
+    int s1 = 0;
+    int s2 = 0;
+
+    // reducing the number to the first two digits of the given number
+    do
+    {
+        start = start / 10;
+    }
+    while (start > 100);
+
+    // s1 is the first digit from the 1st and s2 is the second
+    s2 = start % 10;
+    s1 = start / 10;
+
+    // AMEX Validy of CC
+    if (len == 15 && (s1 == 3 && (s2 == 4 || s2 == 7)))
+    {
+        printf("American Express\n");
+        return 0;
+    }
+
+    // MasterCard Validy of CC
+    if (len == 16 && (s1 == 5 && (s2 == 1 || s2 == 2 || s2 == 3 || s2 == 4 || s2 == 5)))
+    {
+        printf("MasterCard\n");
+        return 0;
+    }
+
+    // VISA Validy of CC
+    if ((len == 13 || len == 16) && s1 == 4)
+    {
+        printf("VISA\n");
+        return 0;
+    }
+
+    else
+    {
+        printf("Not a Valid Credit Card");
+    }
+
+    printf("\n");
+    return 0;
+}
